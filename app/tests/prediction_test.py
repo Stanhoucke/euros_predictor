b17c21c7c1736b9857d1234817e9fdd65a476ab7
Stanley Houcke <stanhoucke@gmail.com>
2021-01-04 13:55:32 +0000

    passed prediction team points test

--- app/tests/prediction_test.py
+++ app/tests/prediction_test.py
@@ -4,37 +4,37 @@ from models.match import Match
 from models.player import Player
 from models.team import Team
 
-class TestResult(unittest.TestCase):
+class TestPrediction(unittest.TestCase):
     def setUp(self):
         self.player_1 = Player("euros@gmail.com", "password1", "John", "Smith", "Hopp Suisse")
 
         self.team_1 = Team(self.player_1, "France", {
-            "played": 1,
+            "played": 0,
             "won": 0,
-            "drawn": 1,
+            "drawn": 0,
             "lost": 0,
-            "for": 2,
-            "against": 2,
+            "for": 0,
+            "against": 0,
             "difference": 0,
-            "points": 1,
+            "points": 0,
             "rank": None
         })
         self.team_2 = Team(self.player_1, "Germany", {
-            "played": 1,
+            "played": 0,
             "won": 0,
-            "drawn": 1,
+            "drawn": 0,
             "lost": 0,
-            "for": 2,
-            "against": 2,
+            "for": 0,
+            "against": 0,
             "difference": 0,
-            "points": 1,
+            "points": 0,
             "rank": None
         })
 
         self.match_1 = Match(self.team_1, self.team_2)
 
-        goals = {"home": 2, "away": 2}
-        self.prediction_1 = Prediction(self.player_1, self.match_1, goals)
+        goals_1 = {"home": 2, "away": 2}
+        self.prediction_1 = Prediction(self.player_1, self.match_1, goals_1)
 
     def test_prediction_has_match(self):
         self.assertEqual(self.match_1, self.prediction_1.match)
@@ -45,3 +45,10 @@ class TestResult(unittest.TestCase):
     def test_prediciton_has_goals(self):
         self.assertEqual(2, self.prediction_1.goals["home"])
         self.assertEqual(2, self.prediction_1.goals["away"])
+
+    def test_team_points__returns_1_point_each_for_draw(self):
+        self.prediction_1.team_points()
+        home_points = self.prediction_1.match.team_1.group_info["points"]
+        away_points = self.prediction_1.match.team_2.group_info["points"]
+        self.assertEqual(1, home_points)
+        self.assertEqual(1, away_points)
