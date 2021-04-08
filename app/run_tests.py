0f0092bb6ca07b2d40ee57d83c38ca14e80b116a
Stanhoucke <stanhoucke@gmail.com>
2021-04-08 11:39:29 +0100

    passes player team attribute tests

--- app/run_tests.py
+++ app/run_tests.py
@@ -3,6 +3,7 @@ import unittest
 # Import test classes
 from tests.player_test import TestPlayer
 from tests.team_test import TestTeam
+from tests.player_team_test import TestPlayerTeam
 from tests.league_test import TestLeague
 from tests.group_test import TestGroup
 from tests.match_test import TestMatch
--- app/tests/team_test.py
+++ app/tests/team_test.py
@@ -6,12 +6,7 @@ class TestTeam(unittest.TestCase):
 
     def setUp(self):
         self.team_1 = Team("France")
-
-    def test_team_has_name(self):
-        self.assertEqual("France", self.team_1.name)
-
-    def test_team_has_group_info(self):
-        group_info = {
+        self.group_info = {
             "played": 0,
             "won": 0,
             "drawn": 0,
@@ -22,4 +17,9 @@ class TestTeam(unittest.TestCase):
             "points": 0,
             "rank": None
         }
-        self.assertEqual(group_info, self.team_1.group_info)
+
+    def test_team_has_name(self):
+        self.assertEqual("France", self.team_1.name)
+
+    def test_team_has_group_info(self):
+        self.assertEqual(self.group_info, self.team_1.group_info)
