d22624eb95b79382a9751526a60f8b0d73e53fb6
Stanhoucke <stanhoucke@gmail.com>
2021-05-27 12:24:23 +0100

    player and active player routes

--- app/controllers/player_controller.py
+++ app/controllers/player_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, json, request, jsonify, make_response
 from models.player import Player
 from controllers.auth_controller import login_required
 import repositories.player_repository as player_repository
@@ -28,24 +28,50 @@ def get_player_info(player):
         prediction.match = prediction.match.__dict__
         player_info["predictions"].append(prediction.__dict__)
 
-    return player_info
+    player = player.__dict__
+    player.pop("email")
+    player.pop("password")
+
+    player["leagues"] = player_info["leagues"]
+    player["player_teams"] = player_info["player_teams"]
+    player["predictions"] = player_info["predictions"]
+
+    return player
+
 
 @player_blueprint.route("/players", methods=['GET'])
-# @login_required
 def show_players():
     players = []
     selected_players = player_repository.select_all()
     for player in selected_players:
-        player_info = get_player_info(player)
-
-        player = player.__dict__
-        player.pop("email")
-        player.pop("password")
-
-        player["leagues"] = player_info["leagues"]
-        player["player_teams"] = player_info["player_teams"]
-        player["predictions"] = player_info["predictions"]
-
-        players.append(player)
+        players.append(get_player_info(player))
     
     return make_response(jsonify(players)), 200
+
+@player_blueprint.route("/players/<id>", methods=['GET'])
+def show_player(id):
+    selected_player = player_repository.select(id)
+    player = get_player_info(selected_player)
+    return make_response(jsonify(player)), 200
+
+@player_blueprint.route("/player", methods=['GET'])
+@login_required
+def show_active_player():
+    player_id = show_active_player.user_id
+    if not isinstance(player_id, str):
+        try:
+            active_player = player_repository.select(player_id)
+            player = get_player_info(active_player)
+            return make_response(jsonify(player)), 200
+        except Exception as e:
+                response = {
+                    'status': 'fail',
+                    'message': e
+                }
+                return make_response(jsonify(response)), 200
+    else:
+        response = {
+            'status': 'fail',
+            'message': player_id
+        }
+        return make_response(jsonify(response)), 401
