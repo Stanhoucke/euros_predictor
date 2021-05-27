2442c211410367180953ca1f918a955cc20ea2fb
Stanhoucke <stanhoucke@gmail.com>
2021-05-27 11:15:22 +0100

    show players route refactored

--- app/controllers/player_controller.py
+++ app/controllers/player_controller.py
@@ -5,39 +5,46 @@ import repositories.player_repository as player_repository
 
 player_blueprint = Blueprint("player_blueprint", __name__)
 
+def get_player_info(player):
+    player_info = {
+        "leagues": [],
+        "player_teams": [],
+        "predictions": []
+    }
+    for league in player_repository.leagues(player):
+        player_info["leagues"].append(league.__dict__)
+
+    for player_team in player_repository.player_teams(player):
+        del player_team.player
+        player_team.team = player_team.team.__dict__
+        player_info["player_teams"].append(player_team.__dict__)
+
+    for prediction in player_repository.predictions(player):
+        del prediction.player
+        del prediction.match.team_1.group_info
+        del prediction.match.team_2.group_info
+        prediction.match.team_1 = prediction.match.team_1.__dict__
+        prediction.match.team_2 = prediction.match.team_2.__dict__
+        prediction.match = prediction.match.__dict__
+        player_info["predictions"].append(prediction.__dict__)
+
+    return player_info
+
 @player_blueprint.route("/players", methods=['GET'])
 # @login_required
-def players():
+def show_players():
     players = []
     selected_players = player_repository.select_all()
     for player in selected_players:
-        leagues = []
-        for league in player_repository.leagues(player):
-            leagues.append(league.__dict__)
-
-        player_teams = []
-        for player_team in player_repository.player_teams(player):
-            del player_team.player
-            player_team.team = player_team.team.__dict__
-            player_teams.append(player_team.__dict__)
-
-        predictions = []
-        for prediction in player_repository.predictions(player):
-            del prediction.player
-            del prediction.match.team_1.group_info
-            del prediction.match.team_2.group_info
-            prediction.match.team_1 = prediction.match.team_1.__dict__
-            prediction.match.team_2 = prediction.match.team_2.__dict__
-            prediction.match = prediction.match.__dict__
-            predictions.append(prediction.__dict__)
+        player_info = get_player_info(player)
 
         player = player.__dict__
         player.pop("email")
         player.pop("password")
 
-        player["leagues"] = leagues
-        player["player_teams"] = player_teams
-        player["predictions"] = predictions
+        player["leagues"] = player_info["leagues"]
+        player["player_teams"] = player_info["player_teams"]
+        player["predictions"] = player_info["predictions"]
 
         players.append(player)
     
