bf12b1ddc86921516c9dca0c8261df2a4eda98f5
Stanhoucke <stanhoucke@gmail.com>
2021-06-09 14:18:21 +0100

    added player point update function

--- app/api/fetch_results.py
+++ app/api/fetch_results.py
@@ -1,41 +1,42 @@
 import sys
 sys.path.append('/Users/stanleyhoucke/coding_projects/euros_predictor/app/')
 
+import os
+from dotenv import load_dotenv
 import json
 import requests
 import repositories.match_repository as match_repository
 import repositories.team_repository as team_repository
 import repositories.group_repository as group_repository
 
+def fetch_results():
+    load_dotenv()
 
-url = "https://v3.football.api-sports.io/fixtures?league=4&season=2016"
+    url = "https://v3.football.api-sports.io/fixtures?league=4&season=2016"
+    api_key = os.environ.get('FOOTBALL_API_KEY')
 
-payload={}
-headers = {
-  'x-rapidapi-key': 'ef86cf2f6eabdec8263cf20a190a764e',
-  'x-rapidapi-host': 'v3.football.api-sports.io'
-}
+    payload={}
+    headers = {
+    'x-rapidapi-key': api_key,
+    'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
 
-response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=headers, data=payload)
 
-data = json.loads(response.text)
-# print(data["response"][0]["goals"]["home"])
+    data = json.loads(response.text)
+    # print(data["response"][0]["goals"]["home"])
 
-all_matches = match_repository.select_all()
+    all_matches = match_repository.select_all()
 
-for match in all_matches:
-    for item in data["response"]:
-        if item["teams"]["home"]["name"] == "Rep. Of Ireland":
-            item["teams"]["home"]["name"] = "Republic of Ireland"
-        elif item["teams"]["away"]["name"] == "Rep. Of Ireland":
-            item["teams"]["away"]["name"] = "Republic of Ireland"
+    for match in all_matches:
+        for item in data["response"]:
+            if item["teams"]["home"]["name"] == "Rep. Of Ireland":
+                item["teams"]["home"]["name"] = "Republic of Ireland"
+            elif item["teams"]["away"]["name"] == "Rep. Of Ireland":
+                item["teams"]["away"]["name"] = "Republic of Ireland"
 
-        if item["teams"]["home"]["name"] == match.team_1.name and item["teams"]["away"]["name"] == match.team_2.name:
-            match.goals["home"] = item["goals"]["home"]
-            # print(item["goals"]["home"])
-            match.goals["away"] = item["goals"]["away"]
-            match_repository.update(match)
-
-
-
-# if data["response"]["teams"]["name"] == 
+            if item["teams"]["home"]["name"] == match.team_1.name and item["teams"]["away"]["name"] == match.team_2.name:
+                match.goals["home"] = item["goals"]["home"]
+                # print(item["goals"]["home"])
+                match.goals["away"] = item["goals"]["away"]
+                match_repository.update(match)
---
+++ app/helpers/game_functions.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('/Users/stanleyhoucke/coding_projects/euros_predictor/app/')
+
+import repositories.player_repository as player_repository
+from api.fetch_results import fetch_results
+
+def setup_new_player():
+    pass
+
+def update_all_player_points():
+    # Fetch latest results from api
+    fetch_results()
+    # Get all players
+    all_players = player_repository.select_all()
+    # For each player, get predictions
+    for player in all_players:
+        # reset points to 0
+        player.points = 0
+        predictions = player_repository.predictions(player)
+        if len(predictions) > 0:
+            for prediction in predictions: 
+                if prediction.goals["home"] and prediction.goals["away"] and prediction.match.goals["home"] and prediction.match.goals["away"]:
+                    # get points from each prediction
+                    prediction.award_points()
+            # Update player
+            player_repository.update(player)
+
+# update_all_player_points()
