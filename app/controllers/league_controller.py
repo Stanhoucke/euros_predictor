5d72abf9f485a84e25b0d2ae7dc5effbc73a3985
Stanhoucke <stanhoucke@gmail.com>
2021-06-10 17:05:24 +0100

    added players to league json, new league route without auth

--- app/controllers/league_controller.py
+++ app/controllers/league_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify, make_response
+from models.league import League
 import repositories.league_repository as league_repository
 
 league_blueprint = Blueprint("league_blueprint", __name__)
@@ -15,4 +16,20 @@ def show_leagues():
 @league_blueprint.route("/leagues/<id>", methods=['GET'])
 def show_league(id):
     league = league_repository.select(id)
-    return make_response(jsonify(league.__dict__))
+    selected_players = league_repository.players(league)
+    players = []
+    for player in selected_players:
+        del player.email
+        del player.password
+        players.append(player.__dict__)
+    
+    league = league.__dict__
+    league["players"] = players
+    return make_response(jsonify(league))
+
+@league_blueprint.route("/leagues", methods=['POST'])
+def new_league():
+    post_data = request.get_json()
+    league = League(post_data.get('name'))
+    league_repository.save(league)
+    return make_response(jsonify(league.__dict__)), 201
